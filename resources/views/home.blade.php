845e2c0624d2ee4e42bfb55e902207938d55cb5d
Albert <albert.vabe@gmail.com>
2023-12-14 13:56:05 +0100

    Update heme page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,95 +8,94 @@
 
     <div class="main">
         <!-- Services Section -->
-        <section id="soluciones">
-            <div class="container-services">
-                <div class="row">
-                    <div>
-                        <h2 class="section-heading" value-text="homeSoluciones"></h2>
-                        <h3 class="section-subheading text-muted" value-text="homeServicesSubheading"></h3>
-                    </div>
+        <section id="soluciones" class="container-services">
+
+            <div class="row">
+                <div>
+                    <h2 class="section-heading" value-text="homeSoluciones"></h2>
+                    <h3 class="section-subheading text-muted" value-text="homeServicesSubheading"></h3>
                 </div>
-                <div class="row-1">
-                    <div class="row-1-service">
-                        <span class="fa-stack fa-4x">
-                            <span class="fa fa-circle fa-stack-2x text-primary"></span>
-                            <span class="fa fa-check-square-o  fa-stack-1x fa-inverse">
-                                <i class="fas fa-check-square"></i>
-                            </span>
+            </div>
+            <div class="row-1">
+                <div class="row-1-service">
+                    <span class="fa-stack fa-4x">
+                        <span class="fa fa-circle fa-stack-2x text-primary"></span>
+                        <span class="fa fa-check-square-o  fa-stack-1x fa-inverse">
+                            <i class="fas fa-check-square"></i>
                         </span>
-                        <h4 class="service-heading">
-                            <a href="{{ route('biit') }}" value-text="services1"></a>
-                        </h4>
-                    </div>
-                    {{-- outsourcing to delete --}}
-                    {{-- <div class="row-1-service">
+                    </span>
+                    <h4 class="service-heading">
+                        <a href="{{ route('biit') }}" value-text="services1"></a>
+                    </h4>
+                </div>
+                {{-- outsourcing to delete --}}
+                {{-- <div class="row-1-service">
                         <span class="fa-stack fa-4x">
                             <span class="fa fa-circle fa-stack-2x text-primary"></span>
                             <span class="fa fa-users fa-stack-1x fa-inverse"></span>
                         </span>
                         <h4 class="service-heading" value-text="services2"></h4>
                     </div> --}}
-                    <div class="row-1-service">
-                        <span class="fa-stack fa-4x">
-                            <span class="fa fa-circle fa-stack-2x text-primary"></span>
-                            <span class="fa fa-list-ul cart fa-stack-1x fa-inverse"></span>
-                        </span>
-                        <h4 class="service-heading">
-                            <a href="{{ route('consultoria') }}" value-text="services3"></a>
-                        </h4>
-                    </div>
+                <div class="row-1-service">
+                    <span class="fa-stack fa-4x">
+                        <span class="fa fa-circle fa-stack-2x text-primary"></span>
+                        <span class="fa fa-list-ul cart fa-stack-1x fa-inverse"></span>
+                    </span>
+                    <h4 class="service-heading">
+                        <a href="{{ route('consultoria') }}" value-text="services3"></a>
+                    </h4>
                 </div>
-                <div class="row-2">
-                    <div class="row-2-service">
-                        <span class="fa-stack fa-4x">
-                            <span class="fa fa-circle  fa-stack-2x text-primary"></span>
-                            <span class="fa fa-shopping-cart fa-stack-1x fa-inverse"></span>
-                        </span>
-                        <h4 class="service-heading">
-                            <a href="{{ route('biit') }}" value-text="services4"></a>
-                        </h4>
-                    </div>
-                    <div class="row-2-service">
-                        <span class="fa-stack fa-4x">
-                            <span class="fa fa-circle fa-stack-2x text-primary"></span>
-                            <span class="fa fa-briefcase fa-stack-1x fa-inverse"></span>
-                        </span>
-                        <h4 class="service-heading">
-                            <a href="{{ route('biit') }}" value-text="services5"></a>
-                        </h4>
-                    </div>
+            </div>
+            <div class="row-2">
+                <div class="row-2-service">
+                    <span class="fa-stack fa-4x">
+                        <span class="fa fa-circle  fa-stack-2x text-primary"></span>
+                        <span class="fa fa-shopping-cart fa-stack-1x fa-inverse"></span>
+                    </span>
+                    <h4 class="service-heading">
+                        <a href="{{ route('biit') }}" value-text="services4"></a>
+                    </h4>
+                </div>
+                <div class="row-2-service">
+                    <span class="fa-stack fa-4x">
+                        <span class="fa fa-circle fa-stack-2x text-primary"></span>
+                        <span class="fa fa-briefcase fa-stack-1x fa-inverse"></span>
+                    </span>
+                    <h4 class="service-heading">
+                        <a href="{{ route('biit') }}" value-text="services5"></a>
+                    </h4>
                 </div>
             </div>
         </section>
         <!-- Clients Aside -->
-        <section class="clients">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <a href="{{ route('biit') }}">
-                            <img src="img/logos/banner.jpg" class="img-responsive img-centered" alt="banner">
-                        </a>
-                    </div>
+        <section class="container-aside">
+            <div class="aside">
+                <div class="col-lg-12 text-center">
+                    <a href="{{ route('biit') }}">
+                        <img src="img/logos/banner.jpg" class="img-responsive img-centered" alt="banner">
+                    </a>
                 </div>
             </div>
+
         </section>
         <!-- About Section -->
-        <section class="bg-primary" id="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-lg-offset-2 text-center">
-                        <h2 class="section-heading" value-text="aboutHeading"></h2>
-                        <hr class="light">
-                        <h4 class="text-faded" value-text="aboutText"></h4>
-                        <a href="{{ route('biit') }}" class="btn btn-default btn-xl" value-text="aboutButton"></a>
-                    </div>
+        <section id="about" class="container-about">
+
+            <div class="about">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading" value-text="aboutHeading"></h2>
+                    <hr class="light">
+                    <h4 class="text-faded" value-text="aboutText"></h4>
+                    <a href="{{ route('biit') }}" class="btn btn-default btn-xl" value-text="aboutButton"></a>
                 </div>
+
             </div>
         </section>
 
         <!-- Sitemap & Contact Section -->
-        <section id="site">
-            <div class="text-center">
+        <!-- Contact-Custom -->
+        <section id="contact" class="container-contact">
+            <div class="contact">
                 <h2 class="section-heading" value-text="sitemapHeading"></h2>
                 <h3 class="section-subheading text-muted">
                     <span class="fa fa-map-marker" value-text="sitemapAddress"></span>
@@ -107,13 +106,9 @@
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2984.8365965213275!2d2.079309315088584!3d41.572780979247604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a494c6bf0218b9%3A0xfb5a922369909333!2sPymesoft+Vall%C3%A9s+S.L.!5e0!3m2!1ses!2ses!4v1515689921450"
                     width="100%" height="420" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-        </section>
-
-        <!-- Contact-Custom -->
-        <section id="contact">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+            <div class="contact">
+                <div class="contact-text">
+                    <div class="text-center">
                         <h2 class="section-heading" value-text="contactHeading"></h2>
                         <h3 class="section-subheading text-muted">
                             <span class="fa fa-phone" value-text="contactPhone"> </span>
@@ -123,9 +118,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div
-                        class="col-xs-8 col-sm-8 col-md-6 col-lg-6 col-xs-offset-2 col-sm-offset-2 col-md-offset-3 col-lg-offset-3">
+                <div class="contact-text">
+                    <div class="patata">
                         <form id="contact-form" class="form" action="#" method="POST" role="form">
                             <div class="form-group">
                                 <label class="form-label" for="name" value-text="contactName"></label>
