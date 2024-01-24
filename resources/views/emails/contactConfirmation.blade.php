7e46c19e1748cfb574d669a788a5f1171726d9e3
Albert Valls <avalls89@gmail.com>
2024-01-24 21:03:21 +0100

    Updating mail style

--- resources/views/emails/contactConfirmation.blade.php
+++ resources/views/emails/contactConfirmation.blade.php
@@ -28,113 +28,145 @@
             padding: 0;
             margin: 0;
             font-family: 'Bruno Ace', sans-serif;
-            color: pink;
+            color: #17265c;
             background-color: #fafafa;
-            border: 1px solid purple;
+
 
         }
 
-        .mail-header {
+        .color {
+
             background-color: #17265c;
             color: #ffffff;
-            border: 1px solid orange;
-            min-height: 50px;
-            align-items: left;
-            align-self: left;
+        }
+
+        .header {
+            min-height: 80px;
+            height: 80px;
+            max-height: 80px;
+            min-width: 100%;
+            width: 100%;
+            max-width: 100%;
+        }
+
+
+        .center {
+
+            justify-content: space-around;
+            justify-items: center;
+            justify-self: center;
+            align-content: center;
+            align-items: center;
+            align-self: center;
+
+        }
+
+        .center {
+            /* display: flex; */
+            margin: 0 auto;
+            justify-content: space-around;
+
+        }
+
+        .middle {
+            /* display: flex; */
+            vertical-align: middle;
+        }
+
+        .footer {
+            display: flex;
+            min-width: 100%;
+            width: 100%;
+            max-width: 100%;
         }
     </style>
     <title>{{ $dictionary['automatedResponse'] }}</title>
 </head>
 
 <body class="mail">
-    <table cellspacing="1" cellpadding="1" class="mail" style="border: 1px solid black">
-        <tr class="mail-header">
-            <th colspan="1" style="width: 10%;"></th>
-            <th colspan="3" style="width: 35%; text-align: right; font-family: 'Bruno Ace', sans-serif;">
+    <table cellspacing="0" cellpadding="0" class="mail">
+        <tr class="header color">
+            <th colspan="1" style="width: 9%;"></th>
+            <th colspan="3" class="center middle" style="width: 40%;">
                 {{ config('app.name') }}</th>
-            <th colspan="1" style="width: 10%;"></th>
-            <th colspan="3" style="width: 35%; text-align: left;"> Biit </th>
-            <th colspan="1" style="width: 10%; ">
+            <th colspan="1" style="width: 2%;"></th>
+            <th colspan="3" class="center middle" style="width: 40%;"> Biit </th>
+            <th colspan="1" style="width: 9%; ">
             </th>
         </tr>
-        <tr style="border: 1px solid red; align-items: right;
-            align-self: right;  align-content: right;">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3"
-                style="max-width: 35%;  align-items: right;
-            align-self: right;  align-content: center; border: 1px solid red;">
-                <img src="https://media.istockphoto.com/id/1156307040/es/vector/escudo-escolar-dise%C3%B1o-dise%C3%B1o-de-vectores-educativos-ilustraci%C3%B3n-del-emblema-de-la-universidad.jpg?s=612x612&w=0&k=20&c=GhuIqLPgZ71p1wCmZRuLROvTRYqQIYgM9NeqGYnVZAs="
-                    alt="biit"
-                    style="max-width: 50%;   align-items: right;  align-content: right;
-            align-self: right; float: center; border: 1px solid red;">
+        <tr>
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="3" class="center" style="max-width: 40%; ">
+                <img class="center" src="https://i.pinimg.com/736x/9d/1f/43/9d1f434700aea18dfd4b993cc8db7f40.jpg"
+                    alt="biit" style="max-width: 50%;">
             </td>
-            <td colspan="1" style="width:10%;"></td>
-            <td colspan="3"
-                style="max-width: 35%; align-items: right;
-            align-self: right;  align-content: right; float: center; border: 1px solid red;">
-                <img src="https://i.pinimg.com/736x/9d/1f/43/9d1f434700aea18dfd4b993cc8db7f40.jpg" alt="pymesoft"
-                    style="max-width: 50%;  align-items: right;  align-content: right;
-            align-self: right; border: 1px solid red;">
+            <td colspan="1" style="width:2%;"></td>
+            <td colspan="3" class="center" style="max-width: 40%;  ">
+                <img class="center" src="https://i.pinimg.com/736x/9d/1f/43/9d1f434700aea18dfd4b993cc8db7f40.jpg"
+                    alt="pymesoft" style="max-width: 50%; ">
             </td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr>
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="7" style="width:80%;">
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="7" style="width:82%; text-align: center; ">
                 <h2 class="mail">{{ $dictionary['thanksContact'] }}</h2>
             </td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr>
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="7" style="width:80%;">
-                <p class="cursiva" style="font-family:'Bruno Ace', 'Courier New', Courier, monospace ">
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="7" style="width:82%;">
+                <p class="cursiva"
+                    style="font-family:'Bruno Ace', 'Courier New', Courier, monospace; text-align: center;">
                     {{ $dictionary['messageRecibed'] }}</p>
             </td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr>
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="7" style="width:80%;">
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="7" style="width:82%; text-align: center;">
                 <p>{{ $dictionary['messageDetails'] }}</p>
             </td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr class="mail-header">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: right;">{{ $dictionary['subject'] }}</td>
-            <td colspan="1" style="width: 10% text-align: left;;"></td>
-            <td colspan="3" style="width: 35%;">{{ $messages->mailsubject }}</td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="3" style="width: 40%; text-align: right;">{{ $dictionary['subject'] }}</td>
+            <td colspan="1" style="width: 2% text-align: left;;"></td>
+            <td colspan="3" style="width: 40%;">{{ $messages->mailsubject }}</td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr class="mail" style="color: #17265c">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: right;">{{ $dictionary['name'] }}</td>
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: left;">{{ $messages->name }}</td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="3" style="width: 40%; text-align: right;">{{ $dictionary['name'] }}</td>
+            <td colspan="1" style="width: 2%;"></td>
+            <td colspan="3" style="width: 40%; text-align: left;">{{ $messages->name }}</td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr class="mail" style="color: #17265c">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: right;">{{ $dictionary['email'] }}</td>
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: left;">
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="3" style="width: 40%; text-align: right;">{{ $dictionary['email'] }}</td>
+            <td colspan="1" style="width: 2%;"></td>
+            <td colspan="3" style="width: 40%; text-align: left;">
                 <a href="mailto:{{ $messages->email }}" style="color: #17265c">&lt;{{ $messages->email }}&gt;</a>
             </td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <tr class="mail" style="color: #17265c">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="3" style="width: 35%; text-align: right;">{{ $dictionary['message'] }}</td>
-            <td colspan="1" style="width: 10%; text-align: left;"></td>
-            <td colspan="3" style="width: 35%;">{{ $messages->message }}</td>
-            <td colspan="1" style="width: 10%;"></td>
+            <td colspan="1" style="width: 9%;"></td>
+            <td colspan="3" style="width: 40%; text-align: right;">{{ $dictionary['message'] }}</td>
+            <td colspan="1" style="width: 2%; text-align: left;"></td>
+            <td colspan="3" style="width: 40%;">{{ $messages->message }}</td>
+            <td colspan="1" style="width: 9%;"></td>
         </tr>
         <!-- Agrega más filas según sea necesario -->
-        <tr class="mail" style="background-color: red; color: #17265c">
-            <td colspan="1" style="width: 10%;"></td>
-            <td colspan="7" style="width: 70%;">{{ $dictionary['corporativeName'] }}</td>
-            <td colspan="1" style="width: 10%;"></td>
+        <tr class="footer color">
+
+            <td colspan="9" class="center middle" style="width: 100%; text-align: center; ">
+                {{ $dictionary['corporativeName'] }}{{ date('Y') }}
+            </td>
+
         </tr>
     </table>
 </body>
