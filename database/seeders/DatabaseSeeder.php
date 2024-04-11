e2826d9d406905f1837fd8e97286ba35f6ee7c5d
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2024-04-11 19:57:45 +0200

    migration, model, seeder specie finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-		$this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
-		$this->call(RoleUserSeeder::class);
+		  $this->call(RoleSeeder::class);
+      $this->call(UserSeeder::class);
+		  $this->call(RoleUserSeeder::class);
+      $this->call(SpecieSeeder::class);
     }
 }
