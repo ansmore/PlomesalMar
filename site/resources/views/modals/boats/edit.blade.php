3e982d64b076445a1be3cdb547f80435e9c328e3
Albert <avalls89@gmail.com>
2024-05-15 14:02:29 +0200

    Updated views of boats

--- site/resources/views/modals/boats/edit.blade.php
+++ site/resources/views/modals/boats/edit.blade.php
@@ -1,31 +1,39 @@
-<div class="modal fade modal-common" id="editBoatModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header bg-info text-white">
-                <h5 class="modal-title" id="editModalLabel">Editar Barco</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
-                    aria-label="Cerrar"></button>
-            </div>
-            <form id="editBoatForm" action="" method="POST"
-                data-edit-url-template="{{ route('boats.update', [':id', 'language' => app()->getLocale()]) }}">
-                @csrf
-                @method('PUT')
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="boatName" class="form-label">Nombre del Barco</label>
-                        <input type="text" class="form-control" id="boatName" name="name" value="" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="registrationNumber" class="form-label">Número de Registro</label>
-                        <input type="text" class="form-control" id="registrationNumber" name="registration_number"
+<section class="modal fade modal-common" id="editBoatModal" tabindex="-1" aria-labelledby="editModalLabel"
+    aria-hidden="true">
+
+    <div class="modal-dialog modal-lg modal__management__box">
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Cerrar">
+            <i class="fa-solid fa-xmark"></i>
+        </button>
+
+        <div class="modal-content body">
+            <h2 class="modal-title body__title" id="editModalLabel" data-text="editBoat"></h2>
+
+            <article class="modal-header bg-info text-white form">
+                <form id="editBoatForm" action="" method="POST"
+                    data-edit-url-template="{{ route('boats.update', [':id', 'language' => app()->getLocale()]) }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body form__group">
+                        <label for="boatName" class="form-label form__group__content"
+                            data-text="boatsNameModal"></label>
+                        <input type="text" class="form-control form__group__input" id="boatName" name="name"
                             value="" />
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-primary">Actualizar</button>
-                </div>
-            </form>
+                    <div class="form__group">
+                        <label for="registrationNumber" class="form-label form__group__content"
+                            data-text="boatsRegistrationPlateModal"></label>
+                        <input type="text" class="form-control form__group__input" id="registrationNumber"
+                            name="registration_number" value="" />
+                    </div>
+                    <div class="form__group__buttons">
+                        <button type="button" class="btn btn-secondary form__button__back" data-bs-dismiss="modal"
+                            data-text="cancelButton">Cancelar</button>
+                        <button type="submit" class="btn btn-primary form__button__success"
+                            data-text="saveButton">Actualizar</button>
+                    </div>
+                </form>
+            </article>
         </div>
     </div>
-</div>
+</section>
