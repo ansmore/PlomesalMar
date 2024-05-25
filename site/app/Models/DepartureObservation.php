30e755c51514dc9e6c4cf43f4533fc5821ffb4cf
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2024-05-25 16:10:45 +0200

    arreglar seeders modelos y migraciones

--- site/app/Models/DepartureObservation.php
+++ site/app/Models/DepartureObservation.php
@@ -5,20 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DepartureUserObservation extends Model
+class DepartureObservation extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['departure_id', 'user_id', 'observation_id', 'is_observer'];
+    protected $fillable = ['departure_id', 'observation_id'];
 
     public function departure()
     {
         return $this->belongsTo(Departure::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
     }
 
     public function observation()
@@ -26,14 +21,12 @@ class DepartureUserObservation extends Model
         return $this->belongsTo(Observation::class);
     }
     
-    public static function createRelations($departureId, $observationId, $userIds, $allUsers)
+    public static function createRelations($departureId, $observationId)
     {
         foreach ($allUsers as $user) {
             self::create([
                 'departure_id' => $departureId,
-                'user_id' => $user->id,
-                'observation_id' => $observationId,
-                'is_observer' => in_array($user->id, $userIds),
+                'observation_id' => $observationId
             ]);
         }
     }
