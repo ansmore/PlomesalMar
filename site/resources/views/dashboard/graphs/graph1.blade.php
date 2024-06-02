5770b00c31c097e23c815a1f446d86f603fb9e42
Albert <avalls89@gmail.com>
2024-06-02 22:55:49 +0200

    Updating graph styles

--- site/resources/views/dashboard/graphs/graph1.blade.php
+++ site/resources/views/dashboard/graphs/graph1.blade.php
@@ -13,26 +13,31 @@
         @include('components.asideMenu')
 
         <section id="logo" class="row">
-            <form method="GET" action="{{ route('dashboard.graph1', app()->getLocale()) }}" id="filterForm">
-                <select name="species" id="speciesDropdown" class="form-control">
-                    @foreach ($species as $specie)
-                        <option value="{{ $specie->id }}" {{ $specie->id == $selectedSpecieId ? 'selected' : '' }}>
-                            {{ $specie->common_name }} ({{ $specie->scientific_name }})
-                        </option>
-                    @endforeach
-                </select>
+            <article class=" card__graph">
+                <form method="GET" action="{{ route('dashboard.graph1', app()->getLocale()) }}" id="filterForm">
+                    <div class="graph__controls">
+                        <select name="species" id="speciesDropdown" class="form-control select">
+                            @foreach ($species as $specie)
+                                <option value="{{ $specie->id }}"
+                                    {{ $specie->id == $selectedSpecieId ? 'selected' : '' }}>
+                                    {{ $specie->common_name }} ({{ $specie->scientific_name }})
+                                </option>
+                            @endforeach
+                        </select>
 
-                <select name="year" id="yearDropdown" class="form-control">
-                    @foreach ($years as $year)
-                        <option value="{{ $year }}" {{ $year == $selectedYear ? 'selected' : '' }}>
-                            {{ $year }}
-                        </option>
-                    @endforeach
-                </select>
+                        <select name="year" id="yearDropdown" class="form-control select">
+                            @foreach ($years as $year)
+                                <option value="{{ $year }}" {{ $year == $selectedYear ? 'selected' : '' }}>
+                                    {{ $year }}
+                                </option>
+                            @endforeach
+                        </select>
 
-                <button type="submit" class="btn btn-primary form__button__success">Actualizar</button>
-            </form>
+                        <button type="submit" class="btn btn-primary form__button__success">Actualizar</button>
+                    </div>
 
+                </form>
+            </article>
             <article class="box__logo">
                 @include('partials.speciesGraph1', [
                     'data' => $speciesData[$selectedSpecieId] ?? [],
--- site/resources/views/dashboard/graphs/multiGraph.blade.php
+++ site/resources/views/dashboard/graphs/multiGraph.blade.php
@@ -13,107 +13,114 @@
         @include('components.asideMenu')
 
         <section id="logo" class="row">
-            <form id="multiGraphForm1" action="{{ route('dashboard.multiGraph', app()->getLocale()) }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="year1">Seleccione el Año 1:</label>
-                    <select id="year1" name="year1" class="form-control">
-                        @foreach ($years as $year)
-                            <option value="{{ $year }}" {{ $year == $selectedYear1 ? 'selected' : '' }}>
-                                {{ $year }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
 
-                <div class="form-group">
-                    <label for="species_id1">Seleccione la Especie 1:</label>
-                    <select id="species_id1" name="species_id1" class="form-control">
-                        <option value="">Seleccionar</option>
-                        @foreach ($species as $specie)
-                            <option value="{{ $specie->id }}"
-                                {{ isset($selectedSpeciesIds[0]) && $selectedSpeciesIds[0] == $specie->id ? 'selected' : '' }}>
-                                {{ $specie->common_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+            <article class="card__graph">
+                <form id="multiGraphForm1" action="{{ route('dashboard.multiGraph', app()->getLocale()) }}" method="POST">
+                    <div class="graph__multi">
+                        @csrf
+                        <div class="form-group">
+                            <label for="year1" class="content">Selecciona l'any 1:</label>
+                            <select id="year1" name="year1" class="form-control select">
+                                @foreach ($years as $year)
+                                    <option value="{{ $year }}" {{ $year == $selectedYear1 ? 'selected' : '' }}>
+                                        {{ $year }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                <div class="form-group">
-                    <label for="species_id2">Seleccione la Especie 2:</label>
-                    <select id="species_id2" name="species_id2" class="form-control">
-                        <option value="">Seleccionar</option>
-                        @foreach ($species as $specie)
-                            <option value="{{ $specie->id }}"
-                                {{ isset($selectedSpeciesIds[1]) && $selectedSpeciesIds[1] == $specie->id ? 'selected' : '' }}>
-                                {{ $specie->common_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                        <div class="form-group">
+                            <label for="species_id1" class="content">Selecciona l'espècie 1:</label>
+                            <select id="species_id1" name="species_id1" class="form-control select">
+                                <option value="">Seleccionar</option>
+                                @foreach ($species as $specie)
+                                    <option value="{{ $specie->id }}"
+                                        {{ isset($selectedSpeciesIds[0]) && $selectedSpeciesIds[0] == $specie->id ? 'selected' : '' }}>
+                                        {{ $specie->common_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                <div class="form-group">
-                    <label for="species_id3">Seleccione la Especie 3:</label>
-                    <select id="species_id3" name="species_id3" class="form-control">
-                        <option value="">Seleccionar</option>
-                        @foreach ($species as $specie)
-                            <option value="{{ $specie->id }}"
-                                {{ isset($selectedSpeciesIds[2]) && $selectedSpeciesIds[2] == $specie->id ? 'selected' : '' }}>
-                                {{ $specie->common_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                        <div class="form-group">
+                            <label for="species_id2" class="content">Selecciona l'espècie 2:</label>
+                            <select id="species_id2" name="species_id2" class="form-control select">
+                                <option value="">Seleccionar</option>
+                                @foreach ($species as $specie)
+                                    <option value="{{ $specie->id }}"
+                                        {{ isset($selectedSpeciesIds[1]) && $selectedSpeciesIds[1] == $specie->id ? 'selected' : '' }}>
+                                        {{ $specie->common_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                <div class="form-group">
-                    <label for="species_id4">Seleccione la Especie 4:</label>
-                    <select id="species_id4" name="species_id4" class="form-control">
-                        <option value="">Seleccionar</option>
-                        @foreach ($species as $specie)
-                            <option value="{{ $specie->id }}"
-                                {{ isset($selectedSpeciesIds[3]) && $selectedSpeciesIds[3] == $specie->id ? 'selected' : '' }}>
-                                {{ $specie->common_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                        <div class="form-group">
+                            <label for="species_id3" class="content">Selecciona l'espècie 3:</label>
+                            <select id="species_id3" name="species_id3" class="form-control select">
+                                <option value="">Seleccionar</option>
+                                @foreach ($species as $specie)
+                                    <option value="{{ $specie->id }}"
+                                        {{ isset($selectedSpeciesIds[2]) && $selectedSpeciesIds[2] == $specie->id ? 'selected' : '' }}>
+                                        {{ $specie->common_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                <div class="form-group">
-                    <label for="species_id5">Seleccione la Especie 5:</label>
-                    <select id="species_id5" name="species_id5" class="form-control">
-                        <option value="">Seleccionar</option>
-                        @foreach ($species as $specie)
-                            <option value="{{ $specie->id }}"
-                                {{ isset($selectedSpeciesIds[4]) && $selectedSpeciesIds[4] == $specie->id ? 'selected' : '' }}>
-                                {{ $specie->common_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                        <div class="form-group">
+                            <label for="species_id4" class="content">Selecciona l'espècie 4:</label>
+                            <select id="species_id4" name="species_id4" class="form-control select">
+                                <option value="">Seleccionar</option>
+                                @foreach ($species as $specie)
+                                    <option value="{{ $specie->id }}"
+                                        {{ isset($selectedSpeciesIds[3]) && $selectedSpeciesIds[3] == $specie->id ? 'selected' : '' }}>
+                                        {{ $specie->common_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                <button type="button" id="updateGraphButton1" class="btn btn-primary">Actualizar Gráfico 1</button>
-            </form>
+                        <div class="form-group">
+                            <label for="species_id5" class="content">Selecciona l'espècie 5:</label>
+                            <select id="species_id5" name="species_id5" class="form-control select">
+                                <option value="">Seleccionar</option>
+                                @foreach ($species as $specie)
+                                    <option value="{{ $specie->id }}"
+                                        {{ isset($selectedSpeciesIds[4]) && $selectedSpeciesIds[4] == $specie->id ? 'selected' : '' }}>
+                                        {{ $specie->common_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-            <form id="multiGraphForm2" action="{{ route('dashboard.multiGraph', app()->getLocale()) }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="year2">Seleccione el Año 2:</label>
-                    <select id="year2" name="year2" class="form-control">
-                        @foreach ($years as $year)
-                            <option value="{{ $year }}" {{ $year == $selectedYear2 ? 'selected' : '' }}>
-                                {{ $year }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                <button type="button" id="updateGraphButton2" class="btn btn-primary">Actualizar Gráfico 2</button>
-            </form>
+                        <button type="button" id="updateGraphButton1" class="btn btn-primary button__success">Actualitza
+                            gràfic 1</button>
+                    </div>
+                </form>
 
-            <article id="multiGraphContainer">
-                <div class="chart-container">
+                <form id="multiGraphForm2" action="{{ route('dashboard.multiGraph', app()->getLocale()) }}" method="POST">
+                    @csrf
+                    <div class="form-group graph__controls">
+                        <label for="year2" class="content">Selecciona l'any 2:</label>
+                        <select id="year2" name="year2" class="form-control select">
+                            @foreach ($years as $year)
+                                <option value="{{ $year }}" {{ $year == $selectedYear2 ? 'selected' : '' }}>
+                                    {{ $year }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <button type="button" id="updateGraphButton2" class="btn btn-primary button__success">Actualitza el
+                            gràfic
+                            2</button>
+                    </div>
+                </form>
+            </article>
+            <article id="multiGraphContainer" class="multigraph__box">
+                <div class="chart-container graph__box">
                     @include('partials.multiGraph1')
                 </div>
-                <div class="chart-container">
+                <div class="chart-container graph__box">
                     @include('partials.multiGraph2')
                 </div>
             </article>
