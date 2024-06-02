da1c5c0c48b60266478b2bf0c614dab0fa831dfa
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2024-06-02 21:44:37 +0200

    fix test controller

--- site/app/Http/Middleware/VerifyCsrfToken.php
+++ site/app/Http/Middleware/VerifyCsrfToken.php
@@ -17,8 +17,5 @@ class VerifyCsrfToken extends Middleware
         '/en/plomesalmarContact',
         '/ca/plomesalmarContact',
         '/{ca,en,es}/plomesalmarContact',
-        'en/dashboard/multiGraph',
-        'en/dashboard/graph1',
-        'en/dashboard/donutGraph',
     ];
 }
